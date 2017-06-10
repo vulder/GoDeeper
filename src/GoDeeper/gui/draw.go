d9de098cc40beaa4c5a77caa8f8520facedd8bca
Stefan Ganser <ganser@fim.uni-passau.de>
2017-06-11 00:51:00 +0200

    code to create pixel (int) arrays from arbitrary positive ints

--- src/GoDeeper/gui/draw.go
+++ src/GoDeeper/gui/draw.go
@@ -72,8 +72,8 @@ func drawBoard() {
 				break
 			case game.SuperPowerFood:
 				DrawSuperPowerFood(sx, sy)
-        break
-      case game.Wormwhole:
+				break
+			case game.Wormwhole:
 				DrawWormwhole(sx, sy)
 				break
 			}
@@ -83,7 +83,7 @@ func drawBoard() {
 }
 
 /* utils */
-func coordsToScreen(x, y int) (sx, sy int){
+func coordsToScreen(x, y int) (sx, sy int) {
 	sx = x * tile_size
 	sy = y * tile_size
 	return
@@ -94,10 +94,119 @@ func drawVecPoint(p Point) {
 }
 
 type rgb struct {
-	r,g,b uint8
+	r, g, b uint8
 }
 
 func (c rgb) SetGLColor() {
-	gl.ClearColor(255,255,255,0)
+	gl.ClearColor(255, 255, 255, 0)
 	gl.Color3ub(c.r, c.g, c.b)
+}
+
+func num2PixelArray(n int) *[][]int {
+	var nDigits = n%10 + 1
+	var result [][]int = make([][]int, 5)
+	for i := 0; i < 5; i++ {
+		result[i] = make([]int, nDigits * 5)
+	}
+
+	var currDigitPos = nDigits
+
+	for n > 0 {
+		var currArr *[][]int = digit2PixelArray(n % 10)
+		var xOffset int = (currDigitPos - 1) * 5
+		for i:= 0; i < len(*currArr); i++ {
+			for j:= 0; j < len((*currArr)[i]); j++ {
+				result[i][j + xOffset] = (*currArr)[i][j]
+			}
+		}
+		n /= 10
+		currDigitPos -= 1
+	}
+	return &result
+}
+
+func digit2PixelArray(digit int) *[][]int {
+	switch digit {
+	case 0:
+		return &[][]int{
+			[]int{0, 1, 1, 1, 0},
+			[]int{0, 1, 0, 1, 0},
+			[]int{0, 1, 0, 1, 0},
+			[]int{0, 1, 0, 1, 0},
+			[]int{0, 1, 1, 1, 0},
+		}
+	case 1:
+		return &[][]int{
+			[]int{0, 0, 1, 0, 0},
+			[]int{0, 0, 1, 0, 0},
+			[]int{0, 0, 1, 0, 0},
+			[]int{0, 0, 1, 0, 0},
+			[]int{0, 0, 1, 0, 0},
+		}
+	case 2:
+		return &[][]int{
+			[]int{0, 1, 1, 1, 0},
+			[]int{0, 0, 0, 1, 0},
+			[]int{0, 1, 1, 1, 0},
+			[]int{0, 1, 0, 0, 0},
+			[]int{0, 1, 1, 1, 0},
+		}
+	case 3:
+		return &[][]int{
+			[]int{0, 1, 1, 1, 0},
+			[]int{0, 0, 0, 1, 0},
+			[]int{0, 1, 1, 1, 0},
+			[]int{0, 0, 0, 1, 0},
+			[]int{0, 1, 1, 1, 0},
+		}
+	case 4:
+		return &[][]int{
+			[]int{0, 1, 0, 1, 0},
+			[]int{0, 1, 0, 1, 0},
+			[]int{0, 1, 1, 1, 0},
+			[]int{0, 0, 0, 1, 0},
+			[]int{0, 0, 0, 1, 0},
+		}
+	case 5:
+		return &[][]int{
+			[]int{0, 1, 1, 1, 0},
+			[]int{0, 1, 0, 0, 0},
+			[]int{0, 1, 1, 1, 0},
+			[]int{0, 0, 0, 1, 0},
+			[]int{0, 1, 1, 1, 0},
+		}
+	case 6:
+		return &[][]int{
+			[]int{0, 1, 1, 1, 0},
+			[]int{0, 1, 0, 0, 0},
+			[]int{0, 1, 1, 1, 0},
+			[]int{0, 1, 0, 1, 0},
+			[]int{0, 1, 1, 1, 0},
+		}
+	case 7:
+		return &[][]int{
+			[]int{0, 1, 1, 1, 0},
+			[]int{0, 0, 0, 1, 0},
+			[]int{0, 0, 1, 0, 0},
+			[]int{0, 1, 0, 0, 0},
+			[]int{0, 1, 0, 0, 0},
+		}
+	case 8:
+		return &[][]int{
+			[]int{0, 1, 1, 1, 0},
+			[]int{0, 1, 0, 1, 0},
+			[]int{0, 1, 1, 1, 0},
+			[]int{0, 1, 0, 1, 0},
+			[]int{0, 1, 1, 1, 0},
+		}
+	case 9:
+		return &[][]int{
+			[]int{0, 1, 1, 1, 0},
+			[]int{0, 1, 0, 1, 0},
+			[]int{0, 1, 1, 1, 0},
+			[]int{0, 0, 0, 1, 0},
+			[]int{0, 1, 1, 1, 0},
+		}
+	}
+	return nil
 }
